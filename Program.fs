5fa547d01cd87533c65d64b564b45635bd2bb242
Filipe <fc1943s@gmail.com>
2020-03-30 00:35:13 -0300

    Working! This is a good start

--- Program.fs
+++ Program.fs
@@ -5,65 +5,102 @@ open Browser
 open Fable.Core
 open Fable.Core.JsInterop
 
-module JQuery = 
-    [<Emit("window['jQuery']($0)")>]
-    let select : selector:string -> obj = jsNative
+module Core =
+    let setInterval ms fn =
+        let mutable _token = 0
+            
+        _token <- JS.setInterval (fun () ->
+            fun () -> JS.clearInterval _token
+            |> fn
+        ) ms
+
+module JQuery =
+    type Element =
+        { Raw: obj }
+        
+    let rawSelect selector : obj =
+        Dom.window?jQuery selector
+        
+    let setHtml html element =
+        element?html html
+        
+        
+    let trySelect<'T> =
+        rawSelect 
+        >> fun items ->
+            if items?length = 0
+            then None
+            else Some (items?eq 0)
+            
+    let selectItems<'T> =
+        rawSelect
+        >> fun items ->
+            seq {
+                for i in 0 .. items?length - 1 do
+                    items?eq i
+            }
+        
+    let ready fn =
+        (rawSelect "document")?ready fn |> ignore
     
 module Filmow =
-    ()
+    let startAsync = async {
+        let movies = JQuery.rawSelect ".list_items_wrapper .movie_list_item"
+        
+        if movies?length > 0 then
+            ".list_items_wrapper"
+            |> JQuery.rawSelect
+            |> fun x -> x?prepend """
+                <div>
+                    <a href="javascript:void(0);" onclick="window.randomList()">Random</a>
+                    <br/>
+                    "<span id="random_result"></span>"
+                </div> """
+                
+            Core.setInterval 500 (fun _ ->
+                ".btn-lists-infinite-scroll"
+                |> JQuery.trySelect
+                |> function
+                    | None -> ()
+                    | Some button ->
+                        if button?attr "disabled" <> "disabled"
+                        then button?click ()
+            )
+            
+            Dom.window?randomList <- fun () ->
+                let movies = JQuery.rawSelect ".list_items_wrapper .movie_list_item"
+                
+                ".btn-lists-infinite-scroll"
+                |> JQuery.trySelect
+                |> function
+                    | None -> ()
+                    | Some button ->
+                        if button?attr "disabled" <> "disabled" then
+                            "#random_result"
+                            |> JQuery.rawSelect
+                            |> JQuery.setHtml "Load all movies"
+                        else
+                            let random = Random().Next (0, movies?length)
+                            
+                            let movieName =
+                                ".movie_list_item"
+                                |> JQuery.rawSelect
+                                |> fun x -> x?eq(random)?find("img")?attr("alt")
+                            
+                            "#random_result"
+                            |> JQuery.rawSelect
+                            |> JQuery.setHtml (sprintf "%d - %s" random movieName)
+    
+
+    }
 
 module Program =
     printfn "UserScripts.Filmow loaded (Program.fs)"
     
     importAll "./index.scss"
     
-    let movies = JQuery.select ".list_items_wrapper .movie_list_item"
+    JQuery.ready (fun () ->
+        Filmow.startAsync
+        |> Async.StartImmediate
+    )
     
-    if movies?length > 0 then
-        ".list_items_wrapper"
-        |> JQuery.select
-        |> fun x -> x?prepend """
-            <div>
-                <a href="#" onclick="window.randomList()">Random</a>
-                <br/>
-                "<span id="random_result"></span>"
-            </div> """
-            
-        JS.setInterval (fun () ->
-            
-            let scrollButton = JQuery.select ".btn-lists-infinite-scroll"
-            
-            printfn "ATTR: %A" (scrollButton?attr "disabled")
-            
-            if scrollButton?length > 0 && scrollButton?attr "disabled" <> "disabled" then
-                scrollButton?click ()
-                
-        ) 500 |> ignore
-        
-        Dom.window?randomList <- fun () ->
-            let movies = JQuery.select ".list_items_wrapper .movie_list_item"
-            
-            let scrollButton = JQuery.select ".btn-lists-infinite-scroll"
-            
-            if scrollButton?length > 0 && scrollButton?attr "disabled" <> "disabled" then
-                "#random_result"
-                |> JQuery.select
-                |> fun x -> x?html "Load all movies"
-            else
-                printfn "movie len: %A" movies?length
-                
-                let random = Random().Next (0, movies?length - 1)
-                
-                printfn "random: %A" random
-                
-                let movieName =
-                    ".movie_list_item"
-                    |> JQuery.select
-                    |> fun x -> x?eq(random)?find("img")?attr("alt")
-                    |> fun x -> x?substring (1, 2)
-                
-                "#random_result"
-                |> JQuery.select
-                |> fun x -> x?html (sprintf "%d - %s" random movieName)
-    
-
--- index.user.js
+++ index.user.js
@@ -4,7 +4,6 @@
 
 // @match        *://filmow.com/*
 
-// @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @require      dist/app.js?v=1585530888073
 
 // @grant        none
