046afde8e967d73b0952c728ddd353fef5c9e356
Filipe <fc1943s@gmail.com>
2020-03-29 21:44:30 -0300

    Added css

--- index.user.js
+++ index.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         UserScripts.Filmow
-// @version      1585527341942
+// @version      1585528960225
 
 // @match        *://filmow.com/*
 
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
-// @require      dist/app.js?v=1585527341942
+// @require      dist/app.js?v=1585528960225
 
 // @grant        none
 // ==/UserScript==
